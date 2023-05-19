76fa4f63ae477c7c67b735610b645f0acc7d43ae
Andrey Krasnoyarskiy <andrey.krasnoyarskiy@hys-enterprise.com>
2023-05-20 00:50:19 +0300

    add beckend

--- src/app/pages/check-cosmetics/check-cosmetics.component.ts
+++ src/app/pages/check-cosmetics/check-cosmetics.component.ts
@@ -1,5 +1,5 @@
 import { Chart } from 'chart.js';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
 	chartOptions,
 	parseOptions,
@@ -20,101 +20,85 @@ export interface FormData {
 	styleUrls: ['./check-cosmetics.component.scss']
 })
 export class CheckCosmeticsComponent implements OnInit {
-	textarea: HTMLTextAreaElement;
+	resultTextarea: HTMLTextAreaElement;
 	formCount = 1;
-	forms: FormData[] = [{ id: 1, content: '' }];
 	data: any;
-	ordersChart;
-	salesChart;
-	clicked: boolean = true;
-	clicked1: boolean = false;
-	valueExplanationArea: HTMLTextAreaElement;
-	generalDescriptionArea: HTMLTextAreaElement;
-	valueExplanation: string = '';
-	// generalDescription: string = '';
+	compositionChart;
+	// valueExplanation: string = '';
+	someComposition = 'Aqua (Water), Ethylhexyl Stearate, Niacinamide, Glycerin, Persea Gratissima (Avocado) Oil, Theobroma Cacao (Cocoa) Seed Butter Butyrospermum Parkii (Shea Butter), Glyceryl Stearate, Cetearyl Alcohol, Mandelic Acid, Lactobionic Acid, Lactic Acid, Pabthenol, Allantoin, Potassium Cetyl Phosphate, Sodium Stearoyl Glutamate, Ammonium Acryloyldimethyltaurate/VP Copolymer, Disodium EDTA, Ethylhexylglycerin, Phenoxyethanol, DMDM Hydantoin, Parfum (Fragrance), Butylphenyl Methylpropional, Hydroxycitronellal, Limonene, Linalool, Citronellol';
 	riba = `Для состава А6 я бы расставила оценки следующим образом:
+	Питание - 3 (в составе нет особо питательных ингредиентов, таких как масла или богатые витаминами компоненты);
+	Увлажнение - 7 (глицерин находится на 2-ом месте в списке ингредиентов, что говорит о его высоком содержании, и присутствует несколько других увлажняющих компонентов);
+	Борьба с морщинами - 4 (в составе есть ингредиенты, такие как пантенол и витамин С, которые могут помочь в борьбе с морщинами, но они находятся далеко в списке ингредиентов, что может указывать на их низкую концентрацию);
+	Противодействие акне - 8 (в составе есть несколько активных компонентов, таких как экстракт чая, масло чайного дерева и азелоилдиглицинат, которые могут помочь в борьбе с акне);
+		Защита от солнца - 1 (в составе нет ингредиентов, обеспечивающих защиту от ультрафиолетовых лучей);
+		Очищение - 8 (в составе есть несколько очищающих ингредиентов, таких как натрия лауретсульфат, кокамидопропилбетаин и сульфат натрия кокоамфоацетат);
+		Успокоение - 5 (масло чайного дерева и экстракт зеленого чая могут помочь успокоить раздраженную кожу, но они находятся далеко в списке ингредиентов);
+		Отшелушивание - 3 (несмотря на присутствие некоторых кислот, таких как аскорбиновая кислота и лактобионовая кислота, они находятся далеко в списке ингредиентов, что указывает на низкую концентрацию);
+		Разглаживание - 5 (в составе есть несколько ингредиентов, таких как пантенол и глицерин, которые могут помочь разгладить кожу, но они находятся далеко в списке ингредиентов);
+		Осветление - 4 (в составе есть витамин С, который может помочь осветлить пигментацию, но он находится далеко в списке ингредиентов, что указывает на низкую концентрацию);`;
 
-Питание - 3 (в составе нет особо питательных ингредиентов, таких как масла или богатые витаминами компоненты);
-Увлажнение - 7 (глицерин находится на 2-ом месте в списке ингредиентов, что говорит о его высоком содержании, и присутствует несколько других увлажняющих компонентов);
-Борьба с морщинами - 4 (в составе есть ингредиенты, такие как пантенол и витамин С, которые могут помочь в борьбе с морщинами, но они находятся далеко в списке ингредиентов, что может указывать на их низкую концентрацию);
-Противодействие акне - 8 (в составе есть несколько активных компонентов, таких как экстракт чая, масло чайного дерева и азелоилдиглицинат, которые могут помочь в борьбе с акне);
-Защита от солнца - 1 (в составе нет ингредиентов, обеспечивающих защиту от ультрафиолетовых лучей);
-Очищение - 8 (в составе есть несколько очищающих ингредиентов, таких как натрия лауретсульфат, кокамидопропилбетаин и сульфат натрия кокоамфоацетат);
-Успокоение - 5 (масло чайного дерева и экстракт зеленого чая могут помочь успокоить раздраженную кожу, но они находятся далеко в списке ингредиентов);
-Отшелушивание - 3 (несмотря на присутствие некоторых кислот, таких как аскорбиновая кислота и лактобионовая кислота, они находятся далеко в списке ингредиентов, что указывает на низкую концентрацию);
-Разглаживание - 5 (в составе есть несколько ингредиентов, таких как пантенол и глицерин, которые могут помочь разгладить кожу, но они находятся далеко в списке ингредиентов);
-Осветление - 4 (в составе есть витамин С, который может помочь осветлить пигментацию, но он находится далеко в списке ингредиентов, что указывает на низкую концентрацию);`;
-
+	compositionForms: FormData[] = [{ id: 1, content: this.someComposition }];
 	constructor(
 		public openAIService: OpenAIService,
 	) { }
 
 	ngOnInit(): void {
-		this.textarea = document.getElementById('generalDescription') as HTMLTextAreaElement;
-		const chartOrders = document.getElementById('chart-orders');
-		this.valueExplanationArea = document.getElementById('valueExplanation') as HTMLTextAreaElement;
-		this.generalDescriptionArea = document.getElementById('generalDescription') as HTMLTextAreaElement;
-
-
+		this.resultTextarea = document.getElementById('resultTextarea') as HTMLTextAreaElement;
+		const chart = document.getElementById('chart');
 		parseOptions(Chart, chartOptions());
-
-		this.ordersChart = new Chart(chartOrders, {
+		this.compositionChart = new Chart(chart, {
 			type: 'bar',
 			// options: creamAnalyseChart.options,
 			// data: creamAnalyseChart.data
-
 		});
-
-		// ordersChart.options.onHover = (event, chartElement) => {
-
-		//   if (chartElement.length > 0) {
-		//     console.log(event)
-		//     console.log(chartElement)
-		//     const index = chartElement[0]._index;
-		//     this.valueExplanationArea.value = index.toString();
-		//   } else {
-		//     this.valueExplanationArea.value = '';
-		//   }
-		// };
 	}
 
 	addForm() {
 		const newFormId = this.formCount + 1;
 		this.formCount++;
-		this.forms.push({ id: newFormId, content: '' });
+		this.compositionForms.push({ id: newFormId, content: '' });
 	}
 
 	removeForm() {
-		if (this.forms.length > 0) {
-			this.forms.pop();
+		if (this.compositionForms.length > 1) {
+			this.compositionForms.pop();
 			this.formCount--;
 		}
 	}
 
 	updateTextareaRows() {
-		this.textarea.style.height = `${this.textarea.scrollHeight}px`;
+		this.resultTextarea.style.height = `${this.resultTextarea.scrollHeight}px`;
 	}
 
 	updateFormContent(form: FormData, content: string) {
-		form.content = content;
+		// form.content = content;
 	}
 
-	analysisComposition() {
-		this.textarea.value = this.riba;
-		this.ordersChart.options = { ...creamAnalyseChart.options };
-		this.ordersChart.data = { ...creamAnalyseChart.data };
-		this.ordersChart.update();
+	fakeCompositionAnalysis() {
+		this.resultTextarea.value = this.riba;
+		this.compositionChart.options = { ...creamAnalyseChart.options };
+		this.compositionChart.data = { ...creamAnalyseChart.data };
+		this.compositionChart.update();
 		this.updateTextareaRows();
 	}
 
-	realAnalysis() {
-		const prompt = `Rate the cosmetic product from 0-10 based on the following parameters: nutrition, moisturizing, wrinkle smoothing, anti-acne, sun protection, cleansing, soothing, exfoliating, brightening, strengthening, renewing. Include brief descriptions for each rating. Give answer which looks like: Nutrition--range--description~~Moisturizing--range--description~~ and so on. Composition:`;
-		const input = `Aqua (Water), Ethylhexyl Stearate, Niacinamide, Glycerin, Persea Gratissima (Avocado) Oil, Theobroma Cacao (Cocoa) Seed Butter Butyrospermum Parkii (Shea Butter), Glyceryl Stearate, Cetearyl Alcohol, Mandelic Acid, Lactobionic Acid, Lactic Acid, Pabthenol, Allantoin, Potassium Cetyl Phosphate, Sodium Stearoyl Glutamate, Ammonium Acryloyldimethyltaurate/VP Copolymer, Disodium EDTA, Ethylhexylglycerin, Phenoxyethanol, DMDM Hydantoin, Parfum (Fragrance), Butylphenyl Methylpropional, Hydroxycitronellal, Limonene, Linalool, Citronellol`;
+	realCompositionAnalysis() {
+		const prompt = `Rate the cosmetic product from 0 - 10 based on the following parameters: nutrition, moisturizing, wrinkle smoothing, anti - acne, sun protection, cleansing, soothing, exfoliating, brightening, strengthening, renewing.Include brief descriptions for each rating.Give answer which looks like: Nutrition--range--description~~Moisturizing--range--description~~and so on. at the end, add a general description for whom cosmetics with this composition are recommended. Composition: `;
+		// const input = someComposition
+		const input = this.compositionForms[0].content;
 		this.openAIService.getOpenAIResponse(prompt, input).then((result) => {
-			console.log(result);
 			const resultObj = result.split('\n\n');
 			console.log(resultObj);
-			this.generalDescriptionArea.value = result;
+			this.resultTextarea.value = result;
+			this.updateTextareaRows();
 		});
 	}
-}
+
+	clearResult() {
+		this.resultTextarea.value = '';
+		// this.updateTextareaRows();
+		this.resultTextarea.style.height = '157px';
+		this.compositionChart.clear();
+	}
+};
--- src/app/pages/openai.service.ts
+++ src/app/pages/openai.service.ts
@@ -1,56 +1,26 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 
-
-
 @Injectable()
 export class OpenAIService {
-  private apiKey = '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';
 
-  constructor() { }
+	async getOpenAIResponse(prompt: string, inputText: string): Promise<string> {
 
-  async getOpenAIResponse(prompt: string, inputText: string): Promise<string> {
-    const data = {
-      // model: "text-davinci-003",
-      model: "gpt-3.5-turbo",
-      temperature: 0.6,
-      top_p: 1,
-      max_tokens: 1000,
-      frequency_penalty: 0,
-      presence_penalty: 1,
-      messages: [
-        {
-          "role": "system",
-          "content": prompt + inputText
-        },
-      ]
-      // prompt: prompt + inputText
-      // stop: ["\n", " Human:", " AI:"],
-      // input: `${prompt}: ${inputText}`,
-    };
-    const dataToSend = JSON.stringify(data);
+		const dataToSend = JSON.stringify({
+			prompt: prompt,
+			inputText: inputText
+		});
 
-    try {
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', dataToSend, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this.apiKey}`
-        }
-      });
-
-      const result = response.data.choices[0].message.content;
-      console.log(response.data.usage.total_tokens);
-      return result;
-    } catch (error) {
-      console.error('Ошибка при запросе к OpenAI API:', error);
-      throw error;
-    }
-  }
+		try {
+			const response = await axios.post('http://localhost:3000/api/openai-response', dataToSend,
+				{ headers: { 'Content-Type': 'application/json', } }
+			);
+			// console.log(response);
+			const result = response?.data?.result?.choices[0]?.message?.content;
+			return result;
+		} catch (error) {
+			console.error('Ошибка при запросе к бекенду:', error);
+			throw error;
+		}
+	}
 }
-
-
-
-
-
-
-
